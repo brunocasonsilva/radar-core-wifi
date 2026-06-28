43be6fb5bee4c446dd2ba9c155b9ebe845ca329d
Bruno Cason Silva <brunocasonsilva@gmail.com>
2026-06-28 20:36:09 -0300

    Fix: remover useEffect não utilizado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { GoogleMap, LoadScript, Marker, Circle } from '@react-google-maps/api';
 import './App.css';
 
